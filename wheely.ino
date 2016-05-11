717ce5e44c94e0160dda75b2dc1fc92acf079861
skynet1 <thecrazymancan@gmail.com>
2016-05-11 10:42:48 -0500

    Added improved turning accuracy, and started work on state transitions.

--- wheely.ino
+++ wheely.ino
@@ -1,22 +1,38 @@
 /* TODO: Get a much more powerful battery. */
+/* TODO: Get two optical mice, and use their internal sensors. Look into ADNS-2610. */
 const int left_forward_pin=6;
 const int left_backward_pin=5;
 const int right_forward_pin=9;
 const int right_backward_pin=10;
 const int right_wheel_input=A1;
 const int left_wheel_input=A0;
-const int interrupt_pin=0;
-const int led_pin=8;
+const int interrupt_pin=0, interrupt_pin2=1;
+const int left_echo_pin=11, left_trig_pin=12;
+const int right_echo_pin=4, right_trig_pin=3;
 const double body_length=18.0; //cm
 const double wheel_radius=3.5; //cm
 const double max_speed=12.5; //rads / sec
-const double pi=3.141592;
-const int ticks_per_revolution=19;
+const double pi=3.14159265358979;//32384626433832790528841971633993 <- adjust as needed.
+const int ticks_per_revolution=38;
+const double tick_unit=2.0*pi*wheel_radius/double(ticks_per_revolution);
 volatile int num_ticks_left=0, num_ticks_right=0;
 volatile double current_x=0.0, current_y=0.0, current_angle=0.0;
 volatile bool left_direction=true, right_direction=true;
 const int min_pwm=0;
+const int front_an=A5;
 
+void setup_pins()
+{
+	pinMode(left_echo_pin, INPUT);
+	pinMode(left_trig_pin, OUTPUT);
+	pinMode(right_backward_pin, OUTPUT);
+	pinMode(right_forward_pin, OUTPUT);
+	pinMode(left_forward_pin, OUTPUT);
+	pinMode(left_backward_pin, OUTPUT);
+	pinMode(right_wheel_input, INPUT);
+	pinMode(left_wheel_input, INPUT);
+}
+#define DEBUG
 void halt()
 {
 	analogWrite(left_forward_pin, 0);
@@ -45,8 +61,8 @@ void interrupt_handle()
 			num_ticks_right--;
 	}
 
-	double dist_left=2.0*pi*wheel_radius*((double) (left-num_ticks_left))/((double) ticks_per_revolution);
-	double dist_right=2.0*pi*wheel_radius*((double) (right-num_ticks_right))/((double) ticks_per_revolution);
+	double dist_left=((double) (left-num_ticks_left))*tick_unit;
+	double dist_right=((double) (right-num_ticks_right))*tick_unit;
 	double center_distance=(dist_left+dist_right)/2.0;
 
 	current_angle+=(dist_right-dist_left)/body_length;
@@ -58,16 +74,10 @@ void interrupt_handle()
 
 void setup()
 {
-	pinMode(right_backward_pin, OUTPUT);
-	pinMode(right_forward_pin, OUTPUT);
-	pinMode(left_forward_pin, OUTPUT);
-	pinMode(left_backward_pin, OUTPUT);
-	pinMode(right_wheel_input, INPUT);
-	pinMode(left_wheel_input, INPUT);
-	pinMode(led_pin, OUTPUT);
-	analogWrite(led_pin, 0);
+	setup_pins();
 	halt();
 	attachInterrupt(interrupt_pin, interrupt_handle, CHANGE);
+	attachInterrupt(interrupt_pin2, interrupt_handle, HIGH);
 	interrupts();
 	delay(1000);
 	#ifdef DEBUG
@@ -101,7 +111,6 @@ void control_wheels(double velocity, double turn_rate)
 	left_direction=left > 0 ? true : false;
 	right_direction = right > 0 ? true : false;
 
-	halt();
 	analogWrite(left_forward_pin, left > 0 ? left : 0);
 	analogWrite(left_backward_pin, left <= 0 ? -left : 0);
 	analogWrite(right_forward_pin, right > 0 ? right : 0);
@@ -143,7 +152,7 @@ void turn(double angle)
 	set_angle(fix_angle(current_angle+angle));
 }
 
-void goto_goal(double desired_x, double desired_y)
+int goto_goal(double desired_x, double desired_y, int (*exit_func) ())
 {
 	double dx=desired_x-current_x, dy=desired_y-current_y;
 	double distance=sqrt(dx*dx+dy*dy);
@@ -151,22 +160,69 @@ void goto_goal(double desired_x, double desired_y)
 	const double max_angle_error=2.0*pi/double(ticks_per_revolution);
 	double eangle=fix_angle(atan2(dy, dx)-current_angle);
 	while (distance > max_dist) {
+		int ret=exit_func();
+		if (ret!=0) {
+			halt();
+			return ret;
+		}
+
 		if (abs(eangle) > max_angle_error) {
 			halt();
 			set_angle(atan2(dy, dx));
 		}
-		control_wheels(-100.0, 0.0);
+		control_wheels(-10.0, 0.0);
 		dx=desired_x-current_x; dy=desired_y-current_y;
 		distance=sqrt(dx*dx+dy*dy);
 		eangle=fix_angle(atan2(dy, dx)-current_angle);
-		delay(10);
+		delay(1);
 	}
 	halt();
+	return 0;
 }
 
+int forward(double distance, int (*exit_func) ()) //cm
+{
+	return goto_goal(current_x+distance*cos(current_angle), current_y+distance*sin(current_angle), exit_func);
+}
+
+double measure_distance(int timeout, int trig_pin, int echo_pin)
+{
+	digitalWrite(trig_pin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(trig_pin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trig_pin, LOW);
+	long int duration=pulseIn(echo_pin, HIGH);
+	double distance = (double(duration)/2)/29.1;
+	return distance;
+}
+
+int until_wall_gone()
+{
+	double d=measure_distance(50.0, left_trig_pin, left_echo_pin);
+	if (d >= 50.0)
+		return 1;
+	else
+		return 0;
+}
+
+int until_wall_appears()
+{
+	double d=measure_distance(50.0, left_trig_pin, left_echo_pin);
+	if (d < 5.0)
+		return 1;
+	else
+		return 0;
+}
 
 void loop()
 {
-	goto_goal(0.0, 100.0);
+	/* goto_goal(50.0, 50.0);
+	goto_goal(-50.0, 50.0);
+	goto_goal(-50.0, -50.0);
+	goto_goal(50.0, -50.0); */
+	forward(50.0, until_wall_gone);
+	turn(pi/2.0);
+	forward(50.0, until_wall_appears);
 	while (1) {}
 }
